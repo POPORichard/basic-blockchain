d97ea14436206b88bdf85f1ab08c4008faf36c6c
wangjingpeng <jingpeng.wang@ppiaas.cn>
2021-10-13 19:18:10 +0800

    v0.3 add CLI

--- main/main.go
+++ main/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"basic-blockchain/CLI"
 	"basic-blockchain/database"
 )
 
@@ -8,17 +9,7 @@ func main(){
 	blockChain := database.NewBlockChain()
 	defer blockChain.Db.Close()
 
+	cli := CLI.CLI{Bc:blockChain}
+	cli.Run()
 
-	blockChain.AddBlock("message 1")
-
-	//for no,block := range blockChain.Blocks{
-	//	fmt.Println("No:",no)
-	//	fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
-	//	fmt.Printf("Data: %s\n", block.Data)
-	//	fmt.Printf("Hash: %x\n", block.Hash)
-	//	pow := handel.NewProofOfWork(block)
-	//	fmt.Printf("nonce: %x\n", block.Nonce)
-	//	fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
-	//	fmt.Println()
-	//}
 }
