a36d3f4d4ebd7d14ba751d405e8f659b702af9a7
wangjingpeng <jingpeng.wang@ppiaas.cn>
2021-10-13 13:34:15 +0800

    add note

--- handel/block.go
+++ handel/block.go
@@ -6,7 +6,7 @@ import (
 	"strconv"
 	"time"
 )
-
+// 计算块hash
 func (block *Block) SetHash() {
 	timestamp := []byte(strconv.FormatInt(block.Timestamp, 10))
 	headers := bytes.Join([][]byte{block.PrevBlockHash, block.Data, timestamp}, []byte{})
@@ -15,12 +15,14 @@ func (block *Block) SetHash() {
 	block.Hash = hash[:]
 }
 
+// 创建新块
 func NewBlock(data string, prevBlockHash []byte) *Block {
 	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}}
 	block.SetHash()
 	return block
 }
 
+// 创建创世块
 func NewGenesisBlock() *Block{
 	return NewBlock("Genesis Block", []byte{})
 }
--- handel/chain.go
+++ handel/chain.go
@@ -1,11 +1,13 @@
 package handel
 
+// 向链上添加块
 func (blockChain *BlockChain) AddBlock(data string) {
 	prevBlock := blockChain.Blocks[len(blockChain.Blocks)-1]
 	newBlock := NewBlock(data, prevBlock.Hash)
 	blockChain.Blocks = append(blockChain.Blocks, newBlock)
 }
 
+// 创建新创世链
 func NewBlockChain() *BlockChain {
 	return &BlockChain{[]*Block{NewGenesisBlock()}}
 }
