6acfbdd628aef13ca8621da25e64811c0183e327
wangjingpeng <jingpeng.wang@ppiaas.cn>
2021-10-14 17:14:16 +0800

    v0.4 add Transactions

--- main/main.go
+++ main/main.go
@@ -2,14 +2,10 @@ package main
 
 import (
 	"basic-blockchain/CLI"
-	"basic-blockchain/database"
 )
 
-func main(){
-	blockChain := database.NewBlockChain()
-	defer blockChain.Db.Close()
-
-	cli := CLI.CLI{Bc:blockChain}
+func main() {
+	cli := CLI.CLI{}
 	cli.Run()
 
 }
